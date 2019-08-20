32a275e89ae12df0406ac2ba2c78b7c21029617b
Henry Munoz <alexcol123@yahoo.com>
2019-08-20 19:36:19 -0400

    Added the channell component

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { Menu } from 'semantic-ui-react';
 import UserPanel from './UserPanel';
+import Channels from './Channels';
 
 export class SidePanel extends Component {
   render() {
-
     const { currentUser } = this.props;
 
     return (
@@ -16,6 +16,7 @@ export class SidePanel extends Component {
         style={{ background: '#4c3c4c', fontSize: '1.2rem' }}
       >
         <UserPanel currentUser={currentUser} />
+        <Channels />
       </Menu>
     );
   }
