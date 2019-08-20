d3eec3756705be65ef131d046e1f1c7e3a67b3f5
Henry Munoz <alexcol123@yahoo.com>
2019-08-20 17:57:54 -0400

    Added Signout functionality  and clar user action

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { Grid, GridRow, Header, Icon, Dropdown } from 'semantic-ui-react';
-import { userInfo } from 'os';
+import React from 'react';
+import firebase from '../../firebase';
+import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
 
-export class UserPanel extends Component {
+class UserPanel extends React.Component {
   dropdownOptions = () => [
     {
       key: 'user',
@@ -11,33 +11,45 @@ export class UserPanel extends Component {
           Signed in as <strong>User</strong>
         </span>
       ),
-
       disabled: true
     },
-
-    { key: 'avatar', text: <span>Change Avatar</span> },
-    { key: 'signout', text: <span>Signed Out</span> }
+    {
+      key: 'avatar',
+      text: <span>Change Avatar</span>
+    },
+    {
+      key: 'signout',
+      text: <span onClick={this.handleSignout}>Sign Out</span>
+    }
   ];
+
+  handleSignout = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => console.log('signed out!'));
+  };
 
   render() {
     return (
       <Grid style={{ background: '#4c3c4c' }}>
         <Grid.Column>
-          <GridRow style={{ padding: '1.2em', margin: 0 }}>
+          <Grid.Row style={{ padding: '1.2em', margin: 0 }}>
             {/* App Header */}
             <Header inverted floated="left" as="h2">
               <Icon name="code" />
               <Header.Content>DevChat</Header.Content>
             </Header>
-          </GridRow>
+          </Grid.Row>
+
+          {/* User Dropdown  */}
+          <Header style={{ padding: '0.25em' }} as="h4" inverted>
+            <Dropdown
+              trigger={<span>User</span>}
+              options={this.dropdownOptions()}
+            />
+          </Header>
         </Grid.Column>
-        {/* User DropDown */}
-        <header style={{ padding: '0.25' }} as="h4" inverted>
-          <Dropdown
-            trigger={<span>User</span>}
-            options={this.dropdownOptions()}
-          />
-        </header>
       </Grid>
     );
   }
